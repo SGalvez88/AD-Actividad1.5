c9f7b6813800e7c5bba7f6f19ef3fda225843784
Deviatespark <sergio.galvez.reguera@iestubalcain.net>
2022-09-29 20:02:37 +0200

    add: lectura xml hasta nodo hoja

--- src/ad1/actividad/Main.java
+++ src/ad1/actividad/Main.java
@@ -1,6 +1,5 @@
 package ad1.actividad;
 
-import java.awt.BorderLayout;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -9,126 +8,55 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class Main {
 
-    private static final String datosPersonales = "datosPersonales";
-    private static final String datosPublicos = "datosPublicos";
-
-    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
-
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-
+    public static void main(String[] args) throws SAXException, IOException {
+        
+        File file = new File("src\\resources\\Personas.xml");
+        
         try {
-
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            File file = new File("src\\resources\\Personas.xml");
-
+            
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder =  documentBuilderFactory.newDocumentBuilder();
             Document document = documentBuilder.parse(file);
-
-            document.getDocumentElement().normalize();
-
-            NodeList personas = document.getElementsByTagName("persona");
-
-            System.out.println("Nodos personas a recorrer: " + personas.getLength());
-
-            for (int i = 0; i < personas.getLength(); i++) {
-
-                Node nodePersona = personas.item(i);
-
-                NodeList datosPersonalesYDatosPublicos = nodePersona.getChildNodes();
-
-                for (int j = 0; j < datosPersonalesYDatosPublicos.getLength(); j++) {
-                   // System.out.println("El nombre del nodo es: " + datosPersonalesYDatosPublicos.item(j).getNodeName());
+            
+            NodeList listaPersonas = document.getElementsByTagName("persona");
+           
+            for (int i = 0; i < listaPersonas.getLength(); i++) {
+               
+                NodeList listaDatosPersonalesPublicos = listaPersonas.item(i).getChildNodes();
+               
+                for (int j = 0; j < listaDatosPersonalesPublicos.getLength(); j++) {
                     
-                    Node nodeDatosPersonalesYDatosPublicos = datosPersonalesYDatosPublicos.item(j);
-                    
-                    //if (datosPersonalesYDatosPublicos.item(j).getNodeName().equals("datosPersonales")) {
-                    if (nodeDatosPersonalesYDatosPublicos.getNodeName().equals("datosPersonales")) {
+                    if(listaDatosPersonalesPublicos.item(j).getNodeName().equals("datosPersonales")){
                         
-                        //NodeList idApellidoDepSalario = datosPersonalesYDatosPublicos.item(j).getChildNodes(); //obtienes un node y sus hijos
+                        NodeList listaDatosPersonales = listaDatosPersonalesPublicos.item(j).getChildNodes();
                         
-                        NodeList idApellido = nodeDatosPersonalesYDatosPublicos.getChildNodes(); //obtienes un node y sus hijos
-
-                        for (int k = 0; k < idApellido.getLength(); k++) {
-                            //if si es apellido o id
-                            System.out.println("El nombre del nodo es: " + idApellido.item(k).getNodeName());
-                            System.out.println("El dato del nodo es: " + idApellido.item(k).getTextContent());
+                        for (int k = 0; k < listaDatosPersonales.getLength(); k++) {
+                            
                         }
-
-                    //} else if (datosPersonalesYDatosPublicos.item(j).getNodeName().equals("datosPublicos")) {
-                    } else if (nodeDatosPersonalesYDatosPublicos.getNodeName().equals("datosPublicos")) {
-
-                        NodeList depSalario = datosPersonalesYDatosPublicos.item(j).getChildNodes(); //obtienes un node y sus hijos
-
-                        for (int k = 0; k < depSalario.getLength(); k++) {
-                           
-                            System.out.println("El nombre del nodo es: " + depSalario.item(k).getNodeName());
-                            System.out.println("El dato del nodo es: " + depSalario.item(k).getTextContent());
-                        }
-
                     }
-
+                    
+                    if(listaDatosPersonalesPublicos.item(j).getNodeName().equals("datosPublicos")){
+                        
+                        NodeList listaDatosPublicos = listaDatosPersonalesPublicos.item(j).getChildNodes();
+                        
+                        for (int k = 0; k < listaDatosPublicos.getLength(); k++) {
+                        
+                        }
+                    }
+                    
                 }
-
-                /*
-                if (nodePersona.getNodeType() == Node.ELEMENT_NODE) {
-
-                    Element elementoPersona = (Element) nodePersona;
-
-                    NodeList NodeListPersonasHijos = elementoPersona.getChildNodes();
-
-                    for (int j = 0; j < NodeListPersonasHijos.getLength(); j++) {
-
-                        Node nodePersonasHijos = NodeListPersonasHijos.item(j);
-
-                        if (nodePersonasHijos.getNodeType() == Node.ELEMENT_NODE) {
-
-                            Element elementoHijoPersona = (Element) nodePersonasHijos;
-
-                            NodeList nodeListDatosHijos = elementoHijoPersona.getChildNodes();
-
-                            for (int k = 0; k < nodeListDatosHijos.getLength(); k++) {
-
-                                Node nodeDatosHijos = nodeListDatosHijos.item(k);
-
-                                if (nodeDatosHijos.getNodeType() == Node.ELEMENT_NODE) {
-                                    
-                                    Element elementoDatosHijos = (Element) nodeDatosHijos;
-                                   
-                                   
-                                        System.out.println(elementoDatosHijos.getElementsByTagName("id").item(0).getTextContent());
-                                        System.out.println(elementoDatosHijos.getElementsByTagName("apellido").item(0).getTextContent());
-                                                                   
-                                        System.out.println(elementoDatosHijos.getElementsByTagName("dep").item(0).getTextContent());
-                                        System.out.println(elementoDatosHijos.getElementsByTagName("salario").item(0).getTextContent());
-                                    
-
-                                }
-
-                            }
-
-                            /*switch (personasHijos.item(j).getNodeName()) {
-                                case caseDatosPersonales:
-                                    System.out.println("Id: " + elementoHijoPersona.getElementsByTagName("id").item(0).getTextContent());
-                                    System.out.println("Apellido: "
-                                            + elementoHijoPersona.getElementsByTagName("apellido").item(0).getTextContent());
-                                    break;
-                                case caseDatosPublicos:
-                                    System.out.println("Departamento: "
-                                            + elementoHijoPersona.getElementsByTagName("dep").item(0).getTextContent());
-                                    System.out.println("Salario: "
-                                            + elementoHijoPersona.getElementsByTagName("salario").item(0).getTextContent());
-                                    break;
-                 */
+                
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
+         
+        } catch (ParserConfigurationException exception) {
+            exception.getMessage();
         }
+        
+        
     }
 }
